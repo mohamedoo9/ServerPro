77f8047d4b97e0451db0af722ddd6cf45b26067b
mohamedoo9 <39798188+mohamedoo9@users.noreply.github.com>
2018-11-11 20:44:23 +0300

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace ServerPro;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+ class Main extends PluginBase{
+ 	
+ 	public function onEnable(){
+ 		$this->getLogger()->info(TextFormat::GREEN."hello its a test");
+ 	 }
+ 	
+ }
